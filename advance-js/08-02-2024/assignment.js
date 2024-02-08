48954f2ef7d98fd821e5d0bb64f0d1571e9fe36b
latesh <128903239+Latesh0520@users.noreply.github.com>
2024-02-08 09:36:07 +0530

    assignment

---
+++ advance-js/08-02-2024/assignment.js
@@ -0,0 +1,7 @@
+function getRestValues(a,b,...c){
+    console.log("🚀 ~ getRestValues ~ a:", a)
+    console.log("🚀 ~ getRestValues ~ b:", b)
+    console.log("🚀 ~ getRestValues ~ c:", c)
+}
+
+getRestValues(1,4,8,86,15,887,2154,554);
